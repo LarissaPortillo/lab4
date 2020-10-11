4b4760aa67220e8055d9806c708d6661ef4eb035
Glitch (hello-webpage) <none>
2020-10-11 17:41:37 +0000

    🌠💕 Checkpoint ./index.html:21940894/13 ./script.js:21940894/24

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-d3.csv("https://cdn.glitch.com/ae0235a1-538a-483e-b9a1-c0773a2dc129%2Fwealth-health-2014.csv?v=1602435508884", d3.autotype)
+d3.csv('https://cdn.glitch.com/ae0235a1-538a-483e-b9a1-c0773a2dc129%2Fwealth-health-2014.csv?v=1602435508884', d3.autotype)
   .then(data=>{
   data=data;
   console.log(data);
