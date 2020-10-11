46a76eb0dca6297da71639aa8484c7b0e217b326
Glitch (hello-webpage) <none>
2020-10-11 23:40:38 +0000

    👾💕 Checkpoint ./script.js:21940894/275

--- script.js
+++ script.js
@@ -3,7 +3,17 @@ d3.csv('https://cdn.glitch.com/ae0235a1-538a-483e-b9a1-c0773a2dc129%2Fwealth-hea
   data=data;
   console.log(data);
   
-  d3.select('.chart').append('svg');
   
+  const width = 400;
+  const height = 300;
+  
+  const svg = d3.select('.chart').append('svg')
+    .attr('width',width)
+    .attr('height', height);
+  
+  const xScale = d3
+    .scaleLinear()
+    .domain(...)
+    .range(...);
   
 })
