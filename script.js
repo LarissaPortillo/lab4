3d02754a8f1a81cd2cae420a1d611910ecf8e741
Glitch (hello-webpage) <none>
2020-10-12 02:45:39 +0000

    🦃🚟 Checkpoint ./script.js:21940894/565

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ d3.csv('https://cdn.glitch.com/ae0235a1-538a-483e-b9a1-c0773a2dc129%2Fwealth-hea
   console.log(data);
   
   const margin = {top:20, left:20, bottom:20, right:20};
-  const width = 400- margin.left - margin.right;
-  const height = 300 - margin.top - margin.bottom;
+  const width = 650- margin.left - margin.right;
+  const height = 500 - margin.top - margin.bottom;
 
-  const svg = d3.select('.chart').append('svg')
-    .attr('width',width +margin.left +margin.right)
-    .attr('height', height +margin.top +margin.bottom)
-    .append('g')
-    .attr('transform','translate('+margin.left+','+margin.right+')');
+  const svg = d3.select(".chart").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+	  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
   
   console.log(d3.extent(data, d=>d.Income));
   console.log(d3.extent(data, d=> d.LifeExpectancy));
@@ -30,6 +30,7 @@ d3.csv('https://cdn.glitch.com/ae0235a1-538a-483e-b9a1-c0773a2dc129%2Fwealth-hea
   .attr('class','income')
   .attr('stroke', 'darkorange')
   .attr('r',5)
-  .attr('cx', d=> xScale(d.Income));
+  .attr('cx', d=> xScale(d.Income))
+  .attr('cy', d=> xScale(d.LifeExpectancy));
   
 })
